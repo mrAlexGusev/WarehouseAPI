d6a1243ee9c0672c234ff3be64992ad914ac960e
mrALEXnobody <scorp_predater@mail.ru>
2024-08-12 23:15:04 +0300

    Добавлены интерфейсы репозиториев для CRUD операций

---
+++ DAL/Data/Repositories/Abstract/IRepository.cs
@@ -0,0 +1,29 @@
+﻿using WarehouseAPI.DAL.Models.Abstract;
+using WarehouseAPI.DAL.Models;
+
+namespace WarehouseAPI.DAL.Data.Repositories.Abstract
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+    }
+
+    public interface IProductRepository : IRepository<Product>
+    {
+        // Дополнительные методы для Product, если необходимо
+    }
+
+    public interface IWarehouseRepository : IRepository<Warehouse>
+    {
+        // Дополнительные методы для Warehouse, если необходимо
+    }
+
+    public interface IStockRepository : IRepository<Stock>
+    {
+        // Дополнительные методы для Stock, если необходимо
+    }
+}
