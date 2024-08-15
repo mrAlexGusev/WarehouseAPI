c453abd76c6721c76e67ba18a421c66fdbdbe4f0
mrALEXnobody <scorp_predater@mail.ru>
2024-08-15 23:10:44 +0300

    Добавлены интерфейсы для CRUD сервисов

---
+++ BL/Services/Interfaces/IWarehouseService.cs
@@ -0,0 +1,13 @@
+﻿using WarehouseAPI.DAL.Models;
+
+namespace WarehouseAPI.BL.Services.Interfaces
+{
+    public interface IWarehouseService
+    {
+        Task<IEnumerable<Warehouse>> GetAllAsync();
+        Task<Warehouse> GetByIdAsync(int id);
+        Task AddAsync(Warehouse entity);
+        Task UpdateAsync(Warehouse entity);
+        Task DeleteAsync(int id);
+    }
+}
