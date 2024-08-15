dd93b0f7a660eec7ab20d33505ae5bcf956bd8d8
mrALEXnobody <scorp_predater@mail.ru>
2024-08-15 23:15:56 +0300

    Добавлена минимальная реализация интерфесов сервисов

---
+++ BL/Services/StockService.cs
@@ -0,0 +1,41 @@
+﻿using WarehouseAPI.BL.Services.Interfaces;
+using WarehouseAPI.DAL.Data.Repositories.Abstract;
+using WarehouseAPI.DAL.Models;
+
+namespace WarehouseAPI.BL.Services
+{
+    public class StockService : IStockService
+    {
+        private readonly IStockRepository _stockRepository;
+
+        public StockService(IStockRepository stockRepository)
+        {
+            _stockRepository = stockRepository;
+        }
+
+        public async Task<IEnumerable<Stock>> GetAllAsync()
+        {
+            return await _stockRepository.GetAllAsync();
+        }
+
+        public async Task<Stock> GetByIdAsync(int id)
+        {
+            return await _stockRepository.GetByIdAsync(id);
+        }
+
+        public async Task AddAsync(Stock entity)
+        {
+            await _stockRepository.AddAsync(entity);
+        }
+
+        public async Task UpdateAsync(Stock entity)
+        {
+            await _stockRepository.UpdateAsync(entity);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await _stockRepository.DeleteAsync(id);
+        }
+    }
+}
---
+++ BL/Services/WarehouseService.cs
@@ -0,0 +1,41 @@
+﻿using WarehouseAPI.BL.Services.Interfaces;
+using WarehouseAPI.DAL.Data.Repositories.Abstract;
+using WarehouseAPI.DAL.Models;
+
+namespace WarehouseAPI.BL.Services
+{
+    public class WarehouseService : IWarehouseService
+    {
+        private readonly IWarehouseRepository _warehouseRepository;
+
+        public WarehouseService(IWarehouseRepository warehouseRepository)
+        {
+            _warehouseRepository = warehouseRepository;
+        }
+
+        public async Task<IEnumerable<Warehouse>> GetAllAsync()
+        {
+            return await _warehouseRepository.GetAllAsync();
+        }
+
+        public async Task<Warehouse> GetByIdAsync(int id)
+        {
+            return await _warehouseRepository.GetByIdAsync(id);
+        }
+
+        public async Task AddAsync(Warehouse entity)
+        {
+            await _warehouseRepository.AddAsync(entity);
+        }
+
+        public async Task UpdateAsync(Warehouse entity)
+        {
+            await _warehouseRepository.UpdateAsync(entity);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await _warehouseRepository.DeleteAsync(id);
+        }
+    }
+}
