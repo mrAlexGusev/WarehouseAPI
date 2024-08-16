3866e850444d905795c8a4a41053952f023794ab
mrALEXnobody <scorp_predater@mail.ru>
2024-08-16 12:08:08 +0300

    Добавлена минимальная реализация API для CRUD методов сервисов

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WarehouseAPI.BL.Services.Interfaces;
+using WarehouseAPI.DAL.Models;
+
+namespace WarehouseAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetAll()
+        {
+            var products = await _productService.GetAllAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetById(int id)
+        {
+            var product = await _productService.GetByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Add(Product product)
+        {
+            await _productService.AddAsync(product);
+            return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+            await _productService.UpdateAsync(product);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await _productService.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
---
+++ API/Controllers/WarehouseController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WarehouseAPI.BL.Services.Interfaces;
+using WarehouseAPI.DAL.Models;
+
+namespace WarehouseAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WarehouseController : ControllerBase
+    {
+        private readonly IWarehouseService _warehouseService;
+
+        public WarehouseController(IWarehouseService warehouseService)
+        {
+            _warehouseService = warehouseService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Warehouse>>> GetAll()
+        {
+            var warehouses = await _warehouseService.GetAllAsync();
+            return Ok(warehouses);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Warehouse>> GetById(int id)
+        {
+            var warehouse = await _warehouseService.GetByIdAsync(id);
+            if (warehouse == null)
+            {
+                return NotFound();
+            }
+            return Ok(warehouse);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Add(Warehouse warehouse)
+        {
+            await _warehouseService.AddAsync(warehouse);
+            return CreatedAtAction(nameof(GetById), new { id = warehouse.Id }, warehouse);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, Warehouse warehouse)
+        {
+            if (id != warehouse.Id)
+            {
+                return BadRequest();
+            }
+            await _warehouseService.UpdateAsync(warehouse);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await _warehouseService.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
