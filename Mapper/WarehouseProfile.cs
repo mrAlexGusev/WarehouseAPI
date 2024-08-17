1c727d646e96dc6c980f9f64afc1804c184634eb
mrALEXnobody <scorp_predater@mail.ru>
2024-08-17 16:26:15 +0300

    Подключение автомаппера к проекту

---
+++ Mapper/WarehouseProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using WarehouseAPI.API.Models;
+using WarehouseAPI.BL.Domain;
+
+namespace WarehouseAPI.Mapper
+{
+    public class WarehouseProfile : Profile
+    {
+        public WarehouseProfile()
+        {
+            CreateMap<WarehouseDTO, WarehouseModel>();
+            CreateMap<WarehouseModel, WarehouseDTO>();
+        }
+    }
+}
