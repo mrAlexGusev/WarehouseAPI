e773a8639484938e4e1a05424a755a5360a19f74
mrALEXnobody <scorp_predater@mail.ru>
2024-08-17 16:14:33 +0300

    Добавлены модели для работы с API

---
+++ API/Models/StockModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WarehouseAPI.API.Models
+{
+    public class StockModel
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int WarehouseId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public ProductModel Product { get; set; }
+        public WarehouseModel Warehouse { get; set; }
+    }
+}
