fc86d0d05e9a9b8e35fa64c1db62c3024784006b
mrALEXnobody <scorp_predater@mail.ru>
2024-08-17 15:58:46 +0300

    Добавление доменных моделей

---
+++ BL/Domain/WarehouseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace WarehouseAPI.BL.Domain
+{
+    public class WarehouseDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string ContactPhone { get; set; }
+    }
+}
